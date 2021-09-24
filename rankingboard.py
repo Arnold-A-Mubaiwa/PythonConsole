8a40a887f9a13aebf9290b13f139c90db7c7b254
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2021-09-24 12:26:53 +0200

    working with Json file

--- rankingboard.py
+++ rankingboard.py
@@ -1,10 +1,13 @@
-import pandas as pd
+import socket
 
-data = {
-   'name': ['James', 'Billy', 'Bob', 'Amy', 'Tom', 'Harry'],
-   'rank': [4, 1, 3, 5, 2, 6]
-}
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
 
-df = pd.DataFrame(data, index=data['name'])
-num=int(input())
-print(df["name"][df['rank']==num])
+while True:
+   data  = mysock.recv(512)
+   if(len(data)<1):
+      break
+   print(data.decode())
+mysock.close()
