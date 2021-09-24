0c3bf66a37de4094d2f82d4542a71972e7f5467d
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2021-09-24 13:15:01 +0200

    multi-dictionaries

--- jsonPyLib.py
+++ jsonPyLib.py
@@ -16,3 +16,20 @@ print(info)
 print('Name:',info['name'])
 print('Hide:',info['email']['hide'])
 
+input = '''[
+    {"id":"001",
+     "x":"2",
+     "name":"Chuck"
+    },
+    {"id":"009",
+     "x":"7",
+     "name":"Chuck"
+    }
+]'''
+
+v = json.loads(input)
+print("User count", len(v))
+for item in v:
+    print('Name', item['name'])
+    print('Id', item['id'])
+    print('Attribute', item['x'])
